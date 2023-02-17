8a43f74a4b721d3aa43e9b3fd5e87c868026da94
Kyan <weiliang.chia@gmail.com>
2023-02-17 16:39:10 +0800

    ReactJs portfolio

---
+++ src/container/Skills/Skills.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'framer-motion';
+
+import { AppWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
+import './Skills.scss';
+
+const Skills = () => {
+  return (
+    <div>Skills</div>
+  )
+}
+
+export default Skills
